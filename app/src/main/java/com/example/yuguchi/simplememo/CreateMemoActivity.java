76972f2e413ec26ad85c92996b69a5af50509027
yuguchi <sun.sun.tai@gmail.com>
2018-10-13 17:19:24 +0900

    ver1.01

--- app/src/main/java/com/example/yuguchi/simplememo/CreateMemoActivity.java
+++ app/src/main/java/com/example/yuguchi/simplememo/CreateMemoActivity.java
@@ -3,18 +3,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.TwoLineListItem;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.UUID;
 
 public class CreateMemoActivity extends AppCompatActivity {
